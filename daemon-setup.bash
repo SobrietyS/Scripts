78adf18e92f1358bb455da3a9bf437767a9090b0
Sobriety S <87849156+SobrietyS@users.noreply.github.com>
2021-08-09 13:45:07 +0800

    Update daemon-setup.bash

--- daemon-setup.bash
+++ daemon-setup.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-clear
 echo -e "------------------------------------------------"
 echo -e "   Sphero Solutions Multicraft Daemon Script    "
 echo -e "              Created by Sobriety S             \n"
@@ -18,7 +17,7 @@ apt update && apt full-upgrade &> /dev/null
 echo -e "[SUCCESS] System updated!"
 
 echo -e "[INFO] Adding Java PPA..."
-add-apt-repository ppa:linuxuprising/java &> /dev/null
+add-apt-repository ppa:linuxuprising/java
 echo -e "[SUCCESS] Java PPA added!"
 
 echo -e "[INFO] Installing required packages..."
@@ -26,7 +25,7 @@ apt install -y apt-transport-https ansible rsync openjdk-16-jdk openjdk-11-jdk o
 echo -e "[SUCCESS] Packages installed!"
 
 echo -e "[INFO] Downloading Multicraft latest version..."
-wget http://www.multicraft.org/download/linux64 -O multicraft.tar.gz
+wget --quiet http://www.multicraft.org/download/linux64 -O multicraft.tar.gz
 
 if [ ! -f ./multicraft.tar.gz ]; then
     echo -e "[FATAL] Failed to download Multicraft."
@@ -44,13 +43,18 @@ rm -rf multicraft && rm -f multicraft.tar.gz
 echo -e "[SUCCESS] Installers removed!"
 
 echo -e "[INFO] Setting up Systemd Multicraft service..."
-wget http://www.multicraft.org/files/multicraft.service -O /etc/systemd/system/multicraft.service && chmod 644 /etc/systemd/system/multicraft.service && systemctl enable multicraft
+wget --quiet http://www.multicraft.org/files/multicraft.service -O /etc/systemd/system/multicraft.service && chmod 644 /etc/systemd/system/multicraft.service && systemctl enable multicraft
 
 if [ ! -f /etc/systemd/system/multicraft.service ]; then
     echo -e "[FATAL] Failed to create Systemd Multicraft service."
     exit 1
 fi
 echo -e "[SUCCESS] Systemd Multicraft service created and enabled!"
+
+echo -e "[INFO] Removing premade jars and templates..."
+rm -rfv /home/minecraft/multicraft/jar/*
+rm -rfv /home/minecraft/multicraft/templates/*
+echo -e "[SUCCESS] Successfully removed!\n"
 
 echo -e "------------------------------------------------"
 echo -e "   Sphero Solutions Multicraft Daemon Script    "
@@ -63,3 +67,5 @@ echo -e "Now: "
 echo -e "- Edit /home/minecraft/multicraft/multicraft.conf, and configure daemon name and memory!"
 echo -e "- Remember to run /home/minecraft/multicraft/bin/multicraft -v restart after making changes!"
 echo -e "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+exit
